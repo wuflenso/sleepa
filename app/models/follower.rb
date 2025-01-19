c38e9976d3dbd6d08e3aacc3c51507353c67a13c
Karim Rahman <karim.rahman@bukalapak.com>
2025-01-19 22:18:36 +0700

    Implement caching in follower model

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,13 +1,24 @@
 class Follower < ApplicationRecord
   belongs_to :user
 
+  CACHE_PREFIX_USER_FOLLOWINGS = 'class_followers-followings-user_id:'.freeze
+  CACHE_EXPIRY_USER_FOLLOWINGS = 1.day
+  CACHE_PREFIX_USER_FOLLOWERS = 'class_followers-followers-user_id:'.freeze
+  CACHE_EXPIRY_USER_FOLLOWERS = 1.day
+
+  after_save :invalidate_cache
+
   class << self
     def get_followers(user_id)
-      self.where(user_id: user_id).where(is_active: true)&.order(followed_at: "desc")
+      Rails.cache.fetch("#{CACHE_PREFIX_USER_FOLLOWERS}#{user_id}", expires_in: CACHE_EXPIRY_USER_FOLLOWERS) do
+        self.where(user_id: user_id).where(is_active: true)&.order(followed_at: "desc")
+      end
     end
 
     def get_user_followings(follower_user_id)
-      self.where(follower_user_id: follower_user_id).where(is_active: true)&.order(followed_at: "desc")
+      Rails.cache.fetch("#{CACHE_PREFIX_USER_FOLLOWINGS}#{follower_user_id}", expires_in: CACHE_EXPIRY_USER_FOLLOWINGS) do
+        self.where(follower_user_id: follower_user_id).where(is_active: true)&.order(followed_at: "desc")
+      end
     end
 
     def get_follower_details(followed_user_id, follower_user_id)
@@ -31,5 +42,10 @@ class Follower < ApplicationRecord
     self.is_active = false
     self.save!
     self
+  end
+
+  def invalidate_cache
+    Rails.cache.delete("#{CACHE_PREFIX_USER_FOLLOWERS}#{self.user_id}")
+    Rails.cache.delete("#{CACHE_PREFIX_USER_FOLLOWINGS}#{self.follower_user_id}")
   end
 end
